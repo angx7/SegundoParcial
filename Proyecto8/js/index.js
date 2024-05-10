a08f89598ad2879d7e563856b48dffb54fe4d485
Ángel Becerra <angelmirocu@gmail.com>
2024-05-09 20:01:22 -0600

    CRUD finalizado

--- Proyecto8/js/index.js
+++ Proyecto8/js/index.js
@@ -1,11 +1,33 @@
 let formulario = document.getElementById("formulario");
+let formularioEditar = document.getElementById("formularioEditar");
 let nombre = document.getElementById("nombre");
 let fecha = document.getElementById("fecha");
 let descripcion = document.getElementById("descripcion");
-
-let tareas = [];
+let idTarea = document.getElementById("idTarea");
 let listaTareas = document.getElementById("listaTareas");
 let btnGuardar = document.getElementById("btnGuardar");
+let btnGuardarEditar = document.getElementById("btnGuardarEditar");
+
+
+let tareas = [
+    {
+        nombre: "Alex",
+        fecha: "2012-12-12",
+        descripcion: "Lavar el auto con agua y jabón."
+    },
+    {
+        nombre: "Santiago",
+        fecha: "2012-12-12",
+        descripcion: "Saca a pasear al perro."
+    },
+    {
+        nombre: "Samantha",
+        fecha: "2024-03-10",
+        descripcion: "Ir a la carne asada."
+    },
+];
+
+
 
 let mostrarTareas = () => {
     listaTareas.innerHTML = "";
@@ -22,7 +44,7 @@ let mostrarTareas = () => {
                 <strong>${tarea.descripcion}</strong>
             </div>
             <div class='col-1 border p-3 text-center'>
-                <button class='btn btn-success' onclick='completarTarea(${index})'>Editar</button>
+                <button class='btn btn-success' data-bs-toggle="modal" data-bs-target="#editarTarea" onclick='editarTarea(${index})'>Editar</button>
             </div>
             <div class='col-1 border p-3 text-center'>
                 <button class='btn btn-danger' onclick='borrarTarea(${index})'>Eliminar</button>
@@ -31,7 +53,7 @@ let mostrarTareas = () => {
         `;
     });
 }
-
+mostrarTareas();
 let AgregarDatos = () => {
     let datos = {
         nombre: nombre.value,
@@ -48,10 +70,32 @@ let cerrarModal = () => {
     btnGuardar.click();
 }
 
+let cerrarModalEditar = () => {
+    btnGuardarEditar.setAttribute("data-bs-dismiss", "modal");
+    btnGuardarEditar.click();
+}
+
 let borrarTarea = (index) => {
     tareas.splice(index, 1);
     mostrarTareas();
 }
+
+let editarTarea = (index) => {
+    nombreEditar.value = tareas[index].nombre;
+    fechaEditar.value = tareas[index].fecha;
+    descripcionEditar.value = tareas[index].descripcion;
+    idTarea.value = index;
+}
+
+formularioEditar.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let indice = idTarea.value;
+    tareas[indice].nombre = nombreEditar.value;
+    tareas[indice].fecha = fechaEditar.value;
+    tareas[indice].descripcion = descripcionEditar.value;
+    cerrarModalEditar();
+    mostrarTareas();
+});
 
 
 formulario.addEventListener("submit", (e) => {
