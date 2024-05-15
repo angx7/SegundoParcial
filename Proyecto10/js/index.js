df88576d95e90080d3047c4be6635726c3f07594
Ángel Becerra <angelmirocu@gmail.com>
2024-05-15 02:37:29 -0600

    Todos los proyectos de la materia

--- Proyecto10/js/index.js
+++ Proyecto10/js/index.js
@@ -1,33 +1,39 @@
-let listaProductos = document.querySelector('#listaProductos');
+let listaProductos = document.querySelector("#listaProductos");
 
 // Obtiene los productos de la API
 // Obtiene produtos
 let obtieneProductos = () => {
-    // Promise o Promesa
-    fetch('https://fakestoreapi.com/products/')
-        .then(res => res.json())
-        .then(productosObtenidos => {
-            console.log("Productos obtenidos ", productosObtenidos);
+  // Promise o Promesa
+  fetch("https://fakestoreapi.com/products/")
+    .then((res) => res.json())
+    .then((productosObtenidos) => {
+      console.log("Productos obtenidos ", productosObtenidos);
 
-            productosObtenidos.forEach((producto, indice) => {
-                console.log("Producto ", producto);
-                listaProductos.innerHTML += `
+      productosObtenidos.forEach((producto) => {
+        console.log("Producto ", producto);
+        listaProductos.innerHTML += `
                 <div class="col-12 col-md-3 col-lg-3">
                     <div class="card">
-                        <img src="${producto.image}" class="card-img-top imagenProducto py-2" alt="...">
+                        <img src="${
+                          producto.image
+                        }" class="card-img-top imagenProducto py-2" alt="...">
                         <div class="card-body">
                             <h5 class="card-title
-                            ">${producto.title.slice(0,25)}</h5>
-                            <p class="card-text">${producto.description.slice(0,100)}</p>
-                            <p class="card-text text-danger">$${producto.price}</p>
+                            ">${producto.title.slice(0, 25)}</h5>
+                            <p class="card-text">${producto.description.slice(
+                              0,
+                              100
+                            )}</p>
+                            <p class="card-text text-danger">$${
+                              producto.price
+                            }</p>
                             <a href="#" class="btn btn-primary">Comprar</a>
                         </div>
                     </div>
                 </div>
                 `;
-            });
-        });
-}
-
+      });
+    });
+};
 
 obtieneProductos();
